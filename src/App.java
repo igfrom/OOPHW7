5dbf3b80a6826ce032d5eae3d7a0a6f76eeda0b7
Igor From <ig.from@yandex.ru>
2023-10-26 20:20:53 +0700

    1

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        ComplexNumberView view = new ComplexNumberView();
+        ComplexNumberController controller = new ComplexNumberController(view);
+        controller.execute();
+    }
+}
---
+++ src/ComplexNumberInterface.java
@@ -0,0 +1,7 @@
+public interface ComplexNumberInterface {
+    ComplexNumber add(ComplexNumber other);
+    ComplexNumber subtract(ComplexNumber other);
+    ComplexNumber multiply(ComplexNumber other);
+    ComplexNumber divide(ComplexNumber other);
+    String toString();
+}
